f592a4a02cf0b5b122404df6f6f6fa1a3a614159
Eduardo <programaciongrupo6.2021@gmail.com>
2022-08-06 02:44:37 -0500

    texto encriptado realizado

--- js/encriptacion.js
+++ js/encriptacion.js
@@ -9,23 +9,40 @@ La letra "u" es convertida para "ufat"
 
 */
 const encriptar = (texto)=>{
-        if(texto.includes('e')){
-            texto =texto.replace('e','enter');
+    texto = texto.replace(' ','/');
+    let arr =[];
+    console.log(arr)
+    for(let i = 0;i<texto.length;i++){
+            arr.push(texto[i]);
         }
-        if(texto.includes('i')){
-            texto = texto.replace('i','imes');
-        } 
-        if(texto.includes('o')){
-            texto = texto.replace('o','ober')
+    for(let i = 0;i<arr.length;i++){
+        if(arr[i]=='a'){
+            arr[i] = 'ai';
+        }else if (arr[i]=='e'){
+            arr[i] = 'enter';
+        }else if (arr[i]=='i'){
+            arr[i] = 'imes';
+        }else if (arr[i]=='o'){
+            arr[i] = 'ober';
+        }else if(arr[i] =='u'){
+            arr[i] = 'ufat';
         }
-        if(texto.includes('a')){
-            texto = texto.replace('a','aio');
-        }
-        if(texto.includes('u')){
-            texto = texto.replace('u','ufat');
-        }
-        return texto;
-           
+    }
+    let textoEncriptado = arr.toString();
+    textoEncriptado = textoEncriptado.replace(/,/g,'');
+    textoEncriptado = textoEncriptado.replace('/',' ');
+    return textoEncriptado;
 }
+console.log(encriptar('hola como estas'))
 
-console.log(encriptar('hola'))
+/*var textoEncriptar = "hola como estas"
+
+var textoEncriptado2 = textoEncriptar.replace(/[aeiou]/g, function (match) {
+  if (match === "a") return "ai"
+  if (match === "e") return "enter"
+  if (match === "i") return "imes"
+  if (match === "o") return "ober"
+  if (match === "u") return "ufat"
+})
+
+console.log(textoEncriptado2)*/
